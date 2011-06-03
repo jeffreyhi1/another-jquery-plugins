1a482331bb96ee8f146bb3ea88d69da92310090d
azbitnev@gmail.com <azbitnev@gmail.com@c4d96747-ff6b-4188-96c9-f8db21942445>
2011-06-03 07:35:52 +0000

    autoheight: added, customselect: added top and bottom items (inside list)

---
+++ trunk/source/jquery.ajp.autoheight.js
@@ -0,0 +1,40 @@
+/*
+	Copyright (c) 2011 Andrey O. Zbitnev (azbitnev@gmail.com)
+	Licensed under the MIT License (LICENSE.txt).
+
+	$Id$
+*/
+
+(function ($) {
+
+	if (!$.ajp) $.ajp = { }
+	$.ajp.autoHeight = { version: '0.1pa' }
+
+	$.fn.extend({
+
+		autoHeight: function (options) {
+
+			var defaults = {
+				effect: function ($el, newHeight, oldHeight) {
+					$el.animate({ height: newHeight + 'px' }, 'slow', 'linear')
+				}
+			}
+
+			opts = $.extend(defaults, options);
+
+			function reviewHeight() {
+				return this.each(function(i, el) {
+					var $el = $(el)
+					var currentHeight = $el.outerHeight()
+					$el.css({ height: 'auto' })
+					var normalHeight = $el.outerHeight()
+					$el.css({ height: currentHeight })
+					opts.effect($el, normalHeight, currentHeight)
+				})
+			}
+
+			return reviewHeight.apply(this)
+		}
+	})
+
+})(jQuery);
