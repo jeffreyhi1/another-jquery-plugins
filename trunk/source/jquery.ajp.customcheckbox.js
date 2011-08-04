565c58bdea522b45deb9574cad4a6d58bb5f5104
azbitnev@gmail.com <azbitnev@gmail.com@c4d96747-ff6b-4188-96c9-f8db21942445>
2011-08-04 07:07:51 +0000

    customcheckbox: more optimizations and fixes

--- trunk/source/jquery.ajp.customcheckbox.js
+++ trunk/source/jquery.ajp.customcheckbox.js
@@ -8,83 +8,49 @@
 (function ($) {
 
 	if (!$.ajp) $.ajp = { }
-	$.ajp.customCheckbox = { version: '0.5pa', installed: false }
+	$.ajp.customCheckbox = { version: '0.6pa' }
 
 	$.fn.extend({
 
 		customCheckbox: function (options) {
 
 			var defaults = {
-				checkboxHeight: 25,
-				radioHeight: 25
+				checkboxHeight: 13,
+				radioHeight: 13
 			}
 
 			var opts = $.extend(defaults, options);
 
 			return this.each(function(i, el) {
-
-				var api = {
-					init: function () {
-						var ctx = this
-						if (el.type == 'checkbox' || el.type == 'radio') {
-							$(el).css('display', 'none').addClass('ajp-customcheckbox')
-							var $span = $('<span class="ajp-customcheckbox-' + el.type + '"></span>')
-							$(el).before($span).change(function () { api.clear() }).change()
-							if (!el.getAttribute('disabled')) {
-								$span[0].onmousedown = this.pushed
-								$span[0].onmouseup = this.check
-							} else {
+				if (el.type == 'checkbox' || el.type == 'radio') {
+					var height = (el.type == 'radio' ? opts.radioHeight : opts.checkboxHeight)
+					var $span = $('<span class="ajp-customcheckbox-' + el.type + '"></span>')
+					$(el).css('display', 'none')
+						.addClass('ajp-customcheckbox')
+						.before($span)
+						.change(function () {
+							if (el.disabled && !$span.hasClass('ajp-customcheckbox-disabled'))
 								$span.addClass('ajp-customcheckbox-disabled')
-							}
-						}
-						if (!$.ajp.customCheckbox.installed) {
-							$(document).mouseup(function () {
-								ctx.clear()
-							})
-							$.ajp.customCheckbox.installed = true
-						}
-					},
-					pushed: function() {
-						var p, e = this.nextSibling
-						if (e.checked == true && e.type == 'checkbox') {
-							p = '0 -' + (opts.checkboxHeight * 3) + 'px'
-						} else if (e.checked == true && e.type == "radio") {
-							p = '0 -' + (opts.radioHeight * 3) + 'px'
-						} else if (e.checked != true && e.type == "checkbox") {
-							p = '0 -' + opts.checkboxHeight + 'px'
-						} else {
-							p = '0 -' + opts.radioHeight + 'px'
-						}
-						$(this).css('background-position', p)
-					},
-					check: function() {
-						var e = this.nextSibling
-						if (e.checked == true && e.type == 'checkbox') {
-							$(this).css('background-position', '0 0')
-							e.checked = false
-						} else {
-							if (e.type == 'checkbox') {
-								$(this).css('background-position', '0 -' + (opts.checkboxHeight * 2) + 'px')
-							} else {
-								$('input[name=' + e.name + ']').css('background-position', '0 0')
-								$(this).css('background-position', '0 -' + (opts.radioHeight * 2) + 'px')
-							}
-							e.checked = true
-						}
-						$(e).change()
-					},
-					clear: function() {
-						$('input.ajp-customcheckbox').each(function () {
-							$(this).prev().css('background-position',
-								(this.checked ?
-									'0 -' + ((this.type == 'radio' ? opts.radioHeight : opts.checkboxHeight) * 2) + 'px'
-								: '0 0')
-							)
+							if (!el.disabled && $span.hasClass('ajp-customcheckbox-disabled'))
+								$span.removeClass('ajp-customcheckbox-disabled')
+							if (el.type == 'radio' && el.name)
+								$('input[name=' + el.name + ']').each(function () {
+									$(this).prev().css('background-position', '0 ' + (this.checked ? -height * 2 : 0) + 'px')
+								})
+							$span.css('background-position', '0 ' + (el.checked ? -height * 2 : 0) + 'px')
 						})
-					}
+						.change()
+					$span.mousedown(function () {
+						if (!el.disabled)
+							$(this).css('background-position', '0 -' + (el.checked ? height * 3 : height) + 'px')
+					})
+					$span.mouseup(function () {
+						if (!el.disabled) {
+							el.checked = !el.checked
+							$(el).change()
+						}
+					})
 				}
-
-				api.init()
 			})
 		}
 	})
