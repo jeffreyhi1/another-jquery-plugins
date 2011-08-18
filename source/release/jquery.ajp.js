68feff1be898dc4ae1944eec09d1f12401dfcac1
azbitnev <azbitnev@gmail.com>
2011-08-18 07:38:37 +0000

    datepicker: complete work with date formats

--- source/release/jquery.ajp.js
+++ source/release/jquery.ajp.js
@@ -669,7 +669,7 @@
 (function ($) {
 
 	if (!$.ajp) $.ajp = { }
-	$.ajp.datepicker = { version: '0.4pa', initialized: false }
+	$.ajp.datepicker = { version: '0.5pa', installed: false }
 
 	$.fn.extend({
 
@@ -677,7 +677,8 @@
 
 			var defaults = {
 
-				dateFormat: 'yyyy-mm-dd',
+				displayFormat: 'yyyy-mm-dd',
+				valueFormat: 'yyyy-mm-dd',
 				value: function ($el) { var d = $el.val(); if (!d) d = new Date(); return d },
 				show: function ($el, $ctl) {
 					$ctl.css({
@@ -691,25 +692,37 @@
 						visibility: 'hidden'
 					})
 				},
-				update: function ($el, date) {
-					$el.val(format(opts.dateFormat, date))
+				update: function ($el, $vel, date) {
+					$vel.val(formatDate(opts.displayFormat, date))
+					$el.val(formatDate(opts.valueFormat, date)).change()
 				},
 
 				months: [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь' ],
+				monthsFmt: [ 'Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря' ],
 				monthsShort: [ 'Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек' ],
 				days: [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ]
 			}
 
-			var opts = $.extend(defaults, options);
+			var opts = $.extend(defaults, options)
 
-			function format(fmt, date) {
+			function indexOf(v) {
+				for(var i = 0; i < this.length; i++)
+					if (this[i] == v)
+						return i
+				return -1
+			}
+
+			if ($.browser.msie && !opts.months.indexOf)
+				Array.prototype.indexOf = indexOf
+
+			function formatDate(fmt, date) {
 				var dateformat = {
 					'd': function (d) { return d.getDate() },
 					'dd': function (d) { var r = d.getDate(); return (r < 10 ? '0' + r : r) },
 					'm': function (d) { return d.getMonth() + 1 },
 					'mm': function (d) { var r = d.getMonth() + 1; return (r < 10 ? '0' + r : r) },
 					'mmm': function (d) { return opts.monthsShort[d.getMonth()] },
-					'mmmm': function (d) { return opts.months[d.getMonth()] },
+					'mmmm': function (d) { return opts.monthsFmt[d.getMonth()] },
 					'yy': function (d) { var r = d.getYear() - ($.browser.msie ? 2000 : 100); return (r < 10 ? '0' + r : r) },
 					'yyyy': function (d) { return d.getYear() + ($.browser.msie ? 0 : 1900) }
 				}
@@ -727,10 +740,51 @@
 				return res
 			}
 
+			function parseDate(fmt, s) {
+				function pInt(v) { return parseInt(v.replace(/^0+/, '')) }
+				function pYear(v) { return pInt(v) + 2000 }
+				function pMonthShort(v) { return opts.monthsShort.indexOf(v) + 1 }
+				function pMonth(v) { return opts.monthsFmt.indexOf(v) + 1 }
+				function parser(rx, d, fld, p) { var m = d.src.match(rx); d.src = m[2]; d[fld] = p(m[1]) }
+				var dateformat = {
+					'd': function (d) { parser(/^(\d\d?)(.*)/, d, 'day', pInt) },
+					'dd': function (d) { parser(/^(\d\d)(.*)/, d, 'day', pInt) },
+					'm': function (d) { parser(/^(\d\d?)(.*)/, d, 'month', pInt) },
+					'mm': function (d) { parser(/^(\d\d)(.*)/, d, 'month', pInt) },
+					'mmm': function (d) { parser(/^(...)(.*)/, d, 'month', pMonthShort) },
+					'mmmm': function (d) { parser(new RegExp('^(' + opts.monthsFmt.join('|') + ')(.*)'), d, 'month', pMonth) },
+					'yy': function (d) { parser(/^(\d\d)(.*)/, d, 'year', pYear) },
+					'yyyy': function (d) { parser(/^(\d\d\d\d)(.*)/, d, 'year', pInt) }
+				}
+				var d = { src: s }
+				while (fmt.length > 0) {
+					var m = fmt.match(/^(yyyy|yy|mmmm|mmm|mm|m|dd|d)(.*)$/, fmt)
+					if (m) {
+						dateformat[m[1]](d)
+						fmt = m[2]
+					} else {
+						d.src = d.src.substr(1)
+						fmt = fmt.substr(1)
+					}
+				}
+				return d
+			}
+
 			return this.each(function(i, el) {
 
 				var $el = $(el)
 				var yearShift = ($.browser.msie ? 0 : 1900)
+
+				var $vel = $('<input type="' + $el.attr('type') + '"/>')
+
+				var attrs = ['class', 'title']
+				for (var a = 0; a < attrs.length; a ++) {
+					var attr = $el.attr(attrs[a])
+					if (attr) $vel.attr(attrs[a], attr)
+				}
+
+				$el.after($vel)
+				$el.css('display', 'none')
 
 				var $control = $( ''
 					+ '<div class="ajp-datepicker">'
@@ -757,8 +811,8 @@
 
 				$('body').append($control)
 
-				if (!$.ajp.datepicker.initialized) {
-					$.ajp.datepicker.initialized = true
+				if (!$.ajp.datepicker.installed) {
+					$.ajp.datepicker.installed = true
 					$(document).find('body:eq(0)').keydown(function (evt) {
 						if (evt.keyCode == 27)
 							$('.ajp-datepicker').css({ visibility: 'hidden' })
@@ -775,9 +829,10 @@
 
 				function getSelectedDate() {
 					var d = (typeof opts.value == 'function' ? opts.value($el) : opts.value)
-					if ($.browser.msie && /^\d+-\d+-\d+$/.test(d))
-						d = d.replace(/-/g, '/')
-					return new Date(d)
+					if (typeof d == 'object' && d instanceof Date)
+						return d
+					d = parseDate(opts.valueFormat, d)
+					return new Date(d.year, d.month - 1, d.day)
 				}
 
 				function visualize(date) {
@@ -807,7 +862,7 @@
 					$control.data('visualized-date', date)
 					$control.find('.ajp-datepicker-month > tbody > tr > td').click(function () {
 						if ($(this).text()) {
-							opts.update($el, new Date($(this).data('date')))
+							opts.update($el, $vel, new Date($(this).data('date')))
 							opts.hide($el, $control)
 						}
 					})
@@ -830,18 +885,22 @@
 				})
 
 				$control.find('.ajp-datepicker-clear').mouseup(function (evt) {
-					$el.val('')
+					$vel.val('')
+					$el.val('').change()
 				})
 
-				$el.attr('readonly', true).click(function () {
+				$vel.attr('readonly', true).click(function () {
 					var vis = ($control.data('ajp-datepicker-visible') == 'yes' ? 'hidden' : 'visible')
 					if (vis == 'visible') {
 						visualize(getSelectedDate())
-						opts.show($el, $control)
+						opts.show($vel, $control)
 					} else {
-						opts.hide($el, $control)
+						opts.hide($vel, $control)
 					}
 				})
+
+				if ($el.val())
+					opts.update($(), $vel, getSelectedDate())
 			})
 		}
 	})
