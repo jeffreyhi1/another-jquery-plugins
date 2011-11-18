8315e00fbc758000ffd2914f9b27563b35ef3c43
azbitnev <azbitnev@gmail.com>
2011-11-18 12:51:00 +0000

    slider: orientation, added; popup: sketch

--- source/jquery.ajp.slider.js
+++ source/jquery.ajp.slider.js
@@ -8,7 +8,7 @@
 (function ($) {
 
 	if (!$.ajp) $.ajp = { }
-	$.ajp.slider = { version: '0.3pa', installed: false, controls: [], serial: 1 }
+	$.ajp.slider = { version: '0.4pa', installed: false, controls: [], serial: 1 }
 
 	$.fn.extend({
 
@@ -18,6 +18,7 @@
 				min: 0.0,
 				max: 1.0,
 				value: 0.0,
+				orientation: 'horizontal',
 				onchange: function (val, $slider) { }
 			}
 
@@ -53,7 +54,7 @@
 				$el.append($sl)
 				$el.append($lln)
 
-				function shift(dx, raiseEvent) {
+				function shiftHorizontal(dx, raiseEvent) {
 					var w = $ln.outerWidth() - $sl.outerWidth()
 					var l = parseInt($sl.css('left')) + dx
 					if (l < 0) l = 0
@@ -61,6 +62,19 @@
 					$lln.css('width', l.toString() + 'px')
 					$sl.css('left', l.toString() + 'px')
 					var val = opts.min + (l/w) * (opts.max - opts.min)
+					$el.data('ajp-slider-value', val)
+					if (raiseEvent === undefined || raiseEvent)
+						opts.onchange(val, $el)
+				}
+
+				function shiftVertical(dy, raiseEvent) {
+					var h = $el.height() - $sl.outerHeight()
+					var t = parseInt($sl.css('top')) + dy
+					if (t < 0) t = 0
+					if (t > h) t = h
+					$lln.css('height', t.toString() + 'px')
+					$sl.css('top', t.toString() + 'px')
+					var val = opts.min + (t/h) * (opts.max - opts.min)
 					$el.data('ajp-slider-value', val)
 					if (raiseEvent === undefined || raiseEvent)
 						opts.onchange(val, $el)
@@ -85,21 +99,32 @@
 					if (mouse.down) {
 						var dx = evt.clientX - mouse.x 
 						var dy = evt.clientY - mouse.y
-						shift(dx)
+						if (opts.orientation == 'horizontal') {
+							shiftHorizontal(dx)
+						} else {
+							shiftVertical(dy)
+						}
 						mouse.x = evt.clientX
 						mouse.y = evt.clientY
 					}
 				})
 
 				function setValue(value, raiseEvent) {
-					var w = $ln.outerWidth() - $sl.outerWidth()
-					var dx = ((value - opts.min) * w) / (opts.max - opts.min)
-					$sl.css('left', 0)
-					shift(dx, raiseEvent)
+					if (opts.orientation == 'horizontal') {
+						var w = $ln.outerWidth() - $sl.outerWidth()
+						var dx = ((value - opts.min) * w) / (opts.max - opts.min)
+						$sl.css('left', 0)
+						shiftHorizontal(dx, raiseEvent)
+					} else {
+						var h = $el.height() - $sl.outerHeight()
+						var dy = ((value - opts.min) * h) / (opts.max - opts.min)
+						$sl.css('top', 0)
+						shiftVertical(dy, raiseEvent)
+					}
 				}
 
 				var id = $.ajp.slider.serial ++
-				$.ajp.slider.controls[id] = setValue
+				$.ajp.slider.controls[id] = { 'setValue': setValue, 'opts': opts }
 				$el.data('ajp-slider-id', id)
 
 				setValue(opts.value)
@@ -111,12 +136,17 @@
 			return {
 				set: function (value, raiseEvent) {
 					ctx.each(function () {
-						var f = $.ajp.slider.controls[$(this).data('ajp-slider-id')]
-						if (f) f(value, raiseEvent)
+						var c = $.ajp.slider.controls[$(this).data('ajp-slider-id')]
+						if (c) c.setValue(value, raiseEvent)
 					})
 				},
 				get: function () {
-					return ctx.data('ajp-slider-value')
+					var c = $.ajp.slider.controls[$(ctx).data('ajp-slider-id')]
+					if (c) {
+						var val = $(ctx).data('ajp-slider-value')
+						return (val === undefined ? c.opts.value : val)
+					}
+					return undefined
 				}
 			}
 		}
