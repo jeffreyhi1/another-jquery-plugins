d0da7e32911308a3e52e01369f960922f9ce19d0
azbitnev <azbitnev@gmail.com>
2012-03-20 11:43:51 +0000

    zippy: added

--- source/release/jquery.ajp.js
+++ source/release/jquery.ajp.js
@@ -253,19 +253,22 @@
 (function ($) {
 
 	if (!$.ajp) $.ajp = { }
-	$.ajp.autoheight = { version: '0.2pa' }
+	$.ajp.autoheight = { version: '0.3pa' }
 
 	$.fn.extend({
 
 		ajp$autoheight: function (options) {
 
-			var defaults = {
+			var opts, defaults = {
 				effect: function ($el, newHeight, oldHeight) {
-					$el.animate({ height: newHeight + 'px' }, 'slow', 'linear')
-				}
+					$el.animate({ height: newHeight + 'px' }, opts.duration, opts.easing, opts.oncomplete($el))
+				},
+				duration: 'slow',
+				easing: 'linear',
+				oncomplete: function ($el) { }
 			}
 
-			var opts = $.extend(defaults, options);
+			opts = $.extend(defaults, options);
 
 			function reviewHeight() {
 				return this.each(function(i, el) {
@@ -3584,3 +3587,68 @@ $.easing['ajp-bounce'] = function(x, t, b, c, d) {
 
 })(jQuery);
 
+/*
+	Copyright (c) 2011 Andrey O. Zbitnev (azbitnev@gmail.com)
+	Licensed under the MIT License (LICENSE.txt).
+
+	$Id$
+*/
+
+(function ($) {
+
+	if (!$.ajp) $.ajp = { }
+	$.ajp.zippy = { version: '0.1pa', required: ['autoheight'] }
+
+	$.fn.extend({
+
+		ajp$zippy: function (options) {
+
+			var opts, defaults = {
+				duration: 300,
+				easing: 'linear',
+				show: function ($c, $a) {
+					$a.data('ajp-zippy-ready', false)
+					$c.css({ visibility: 'visible' }).ajp$autoheight({
+						duration: opts.duration,
+						easing: opts.easing,
+						oncomplete: function () {
+							$a.data('ajp-zippy-ready', true)
+						}
+					})
+				},
+				hide: function ($c, $a) {
+					$a.data('ajp-zippy-ready', false)
+					$c.animate({ height: 0 }, opts.duration, opts.easing, function () {
+						$c.css({ visibility: 'hidden' })
+						$a.data('ajp-zippy-ready', true)
+					})
+				}
+			}
+
+			opts = $.extend(defaults, options);
+
+			return this.each(function(i, el) {
+				var $a = $(el), $c = $a.next()
+				$a.click(function (evt) {
+					if ($a.data('ajp-zippy-ready')) {
+						if ($a.hasClass('ajp-zippy-collapsed')) {
+							$a.removeClass('ajp-zippy-collapsed')
+								.addClass('ajp-zippy-expanded')
+							opts.show($c, $a)
+						} else {
+							$a.removeClass('ajp-zippy-expanded')
+								.addClass('ajp-zippy-collapsed')
+							opts.hide($c, $a)
+						}
+					}
+					evt.preventDefault()
+					evt.stopPropagation()
+					return false
+				})
+				$a.data('ajp-zippy-ready', true)
+				$c.css({ visibility: 'hidden', 'height': 0 })
+			})
+		}
+	})
+
+})(jQuery);
--- tools/ajp-css-compile.php
+++ tools/ajp-css-compile.php
@@ -37,11 +37,32 @@ function mkdataurl($m) {
 	return "url(data:image/png;base64," . base64_encode(file_get_contents($file)) . ")";
 }
 
+class css_packer {
+
+	protected static function replacement($m) {
+		$h = preg_replace('/[\s\r\n\t]+/m', ' ', trim($m[1]));
+		$h = preg_replace('/\s*([:;])\s*/', '$1', $h);
+		$h = preg_replace('/;$/', '', $h);
+		$h = preg_replace('/:0(em|px|pt)$/', ':0', $h);
+		return '{' . $h . '}';
+	}
+
+	public static function pack($css, $keep_comments = true) {
+		$css = preg_replace_callback('/\{((?:[^\}]|[\s\t\r\n])*)\}/m', array('css_packer', 'replacement'), $css);
+		if (!$keep_comments)
+			$css = preg_replace('/\/\*((?:.|[\s\t\r\n])*?)\*\//m', '', $css);
+		$css = preg_replace('/[\r\n]+/', "\n", $css);
+		return trim($css);
+	}
+}
+
 foreach($files as $f) {
 	$path = dirname($f);
 	$css = file_get_contents($f);
-	$result_css .= preg_replace_callback('/url\(([^)]+)\)/', 'mkdataurl', preg_replace('/\t/', '', preg_replace('/\/\*.*\*\//', '', $css)));
+	$result_css .= css_packer::pack($css, false);
 }
+
+$result_css = preg_replace_callback('/url\(([^)]+)\)/', 'mkdataurl', preg_replace('/\t/', '', preg_replace('/\/\*.*\*\//', '', $result_css)));
 
 file_put_contents($result, $result_css);
 
