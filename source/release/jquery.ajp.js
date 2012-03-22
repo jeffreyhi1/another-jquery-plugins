9ac47eba542d0456cd49b0c0b85e0818c2620e80
azbitnev <azbitnev@gmail.com>
2012-03-22 07:24:43 +0000

    zippy: remove dependence of autoheight, make another default show/hide functions

--- source/release/jquery.ajp.js
+++ source/release/jquery.ajp.js
@@ -3597,28 +3597,51 @@ $.easing['ajp-bounce'] = function(x, t, b, c, d) {
 (function ($) {
 
 	if (!$.ajp) $.ajp = { }
-	$.ajp.zippy = { version: '0.1pa', required: ['autoheight'] }
+	$.ajp.zippy = { version: '0.2pa' }
 
 	$.fn.extend({
 
 		ajp$zippy: function (options) {
 
 			var opts, defaults = {
-				duration: 300,
+				event: 'click',
+				duration: 'fast',
 				easing: 'linear',
 				show: function ($c, $a) {
 					$a.data('ajp-zippy-ready', false)
-					$c.css({ visibility: 'visible' }).ajp$autoheight({
-						duration: opts.duration,
-						easing: opts.easing,
-						oncomplete: function () {
-							$a.data('ajp-zippy-ready', true)
-						}
-					})
+					var h, $cc = $c.children('div:eq(0)'),
+						pt = $cc.css('padding-top'),
+						pb = $cc.css('padding-bottom'),
+						hfix = (/^[\.0]+(px|em|pt)?$/.test(pt) && /^[\.0]+(px|em|pt)$/.test(pb))
+					if (hfix) {
+						h = $cc.css({
+							'padding-top': '1px',
+							'padding-bottom': '1px'
+						}).outerHeight() - 2
+						$cc.css({
+							'padding-top': pt,
+							'padding-bottom': pb
+						})
+					} else {
+						h = $cc.outerHeight()
+					}
+					$c.scrollTop(h)
+						.css({ visibility: 'visible' })
+						.animate({
+								height: '' + h + 'px',
+								scrollTop: h
+							},
+							opts.duration,
+							opts.easing,
+							function () {
+								$a.data('ajp-zippy-ready', true)
+							}
+						)
 				},
 				hide: function ($c, $a) {
 					$a.data('ajp-zippy-ready', false)
-					$c.animate({ height: 0 }, opts.duration, opts.easing, function () {
+					var h = $c.children('div:eq(0)').outerHeight()
+					$c.scrollTop(h).animate({ height: 0, scrollTop: h }, opts.duration, opts.easing, function () {
 						$c.css({ visibility: 'hidden' })
 						$a.data('ajp-zippy-ready', true)
 					})
@@ -3629,7 +3652,7 @@ $.easing['ajp-bounce'] = function(x, t, b, c, d) {
 
 			return this.each(function(i, el) {
 				var $a = $(el), $c = $a.next()
-				$a.click(function (evt) {
+				$a.bind(opts.event, function (evt) {
 					if ($a.data('ajp-zippy-ready')) {
 						if ($a.hasClass('ajp-zippy-collapsed')) {
 							$a.removeClass('ajp-zippy-collapsed')
